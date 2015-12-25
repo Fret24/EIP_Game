5e9031caef54db94cc0444ca1bbd7fe94f356b6f
Fret23 <stevedr@gmail.com>
2015-12-25 12:48:08 -0800

    Added copyright. Fixed libraries again.

--- src/eip_game/Model.java
+++ src/eip_game/Model.java
@@ -1,4 +1,8 @@
 /*
+    Copyright (C) 2015, Sam Cooper-Drake, all rights reserved.
+    Bush School EIP 2015/2016 Video Game Development Project
+*/
+/*
 This is the model for my game.
 It has the data and behavors of the game out side of the view.
 */
